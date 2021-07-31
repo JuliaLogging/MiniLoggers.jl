dd22a3f0627226824b4cbf9ccb9afb0980ce0a63
Andrey Oskin <skoffer@arkoniak.com>
2021-07-31 13:48:05 +0300

    fixed format test

--- test/test02_loggerformat.jl
+++ test/test02_loggerformat.jl
@@ -5,8 +5,23 @@ using ReTest
 
 @testset "basic format" begin
     io = IOBuffer()
-    logger = MiniLogger(io, level = MiniLoggers.Debug,
-                        format = "{level} - {module}:{line}: {message}")
+    logger = MiniLogger(io = io, minlevel = MiniLoggers.Debug,
+                        format = "{datetime}:{level}:{module}:{basename}:{filepath}:{line}:{group}:{module}:{id}:{message}")
+    with_logger(logger) do
+        @info "foo"
+        s = String(take!(io))
+        @test !contains(s, "datetime")
+        @test !contains(s, "level")
+        @test !contains(s, "module")
+        @test !contains(s, "basename")
+        @test !contains(s, "filepath")
+        @test !contains(s, "line")
+        @test !contains(s, "group")
+        @test !contains(s, "module")
+        @test !contains(s, "id")
+        @test contains(s, "foo")
+        @test contains(s, "Info")
+    end
 end
 
 end # module
