0b7861cb1b13287c03e50c6301435856d9660215
Documenter.jl <documenter@juliadocs.github.io>
2021-07-31 12:04:10 +0000

    build based on 0549347

---
+++ v0.2.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MiniLoggers","category":"page"},{"location":"#MiniLoggers","page":"Home","title":"MiniLoggers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MiniLoggers]","category":"page"}]
+}
