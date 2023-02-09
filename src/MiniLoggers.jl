0dc33abc442133d88d234f36ddfd7f8077fe5e9c
Andrey Oskin <skoffer@arkoniak.com>
2023-02-09 16:22:29 +0200

    add: initial implementation json logger (#21)

--- src/MiniLoggers.jl
+++ src/MiniLoggers.jl
@@ -5,9 +5,15 @@ using Markdown
 import Logging: AbstractLogger, shouldlog, min_enabled_level, catch_exceptions, handle_message, LogLevel
 using Logging: Warn, Info, Debug, Error, BelowMinLevel, AboveMaxLevel, global_logger, with_logger, default_logcolor
 
-export MiniLogger, global_logger, with_logger
+export MiniLogger, JsonLogger, global_logger, with_logger
 
+# Utils
+include("common.jl")
 include("tokenizer.jl")
+include("modes.jl")
+
+# Loggers
 include("minilogger.jl")
+include("jsonlogger.jl")
 
 end # module
